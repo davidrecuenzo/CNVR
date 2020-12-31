25c90dbafa2cee7b9baa30a947c36910f8c93958
davidrecuenzo <david.recuenzo.bermejo@alumnos.upm.es>
2020-12-31 12:17:25 +0100

    first commit

---
+++ servidor3.php
@@ -0,0 +1,47 @@
+<?php
+
+echo "<h4>SERVIDOR 3</h4>";
+
+// Parámetros de conexión a la base de datos
+$host = "10.1.2.%";
+$db = "cnvr";
+$user = "s3";
+$pass = "xxxx";
+$link = "";
+
+// Conectar al servidor MySQL
+if (!$link = mysql_connect($host,$user,$pass)) {
+    exit("Imposible conectar a la base de datos.");
+}
+
+// Seleccionar base de datos
+if (!mysql_select_db($db,$link)) {
+    exit("Imposible seleccionar base de datos: ".mysql_error($link));
+}
+
+// Seleccionar datos de la tabla "servers"
+$query = "SELECT * FROM servers;";
+if (!$result = mysql_query($query,$link)) {
+    exit("Error al seleccionar datos de la base de datos: ".mysql_error($link));
+}
+
+// Imprimir tabla
+?>
+<p>Ultimos Accesos:</p>
+<table style="border: 1px solid #fff;">
+<tr><th>ID</th><th>Nombre</th></tr>
+<?php
+
+while ($row = mysql_fetch_array($result)) {
+    echo "<tr><td>" . $row['id'] . "</td><td>" . $row['nombre_server'] . "</td></tr>";
+}
+
+?>
+</table>
+<?php
+
+// Cerrar conexión MySQL
+mysql_close();
+
+?>
+<p>-- FIN --</p>
